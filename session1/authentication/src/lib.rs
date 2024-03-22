b4bc53220bb0fe82d6836796a27be5dce6257f2c
Vitaliy Boyanivskyy <vitaliy.boyanivsky@scalors.com>
2024-03-22 19:43:32 +0200

    hash map

--- session1/authentication/src/lib.rs
+++ session1/authentication/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn greet_user(name: &str) -> String {
     format!("Welcome {name}")
 }
@@ -5,15 +7,16 @@ pub fn greet_user(name: &str) -> String {
 #[derive(PartialEq, Debug)]
 pub enum LoginAction {
     Granted(LoginRole),
-    Denied
+    Denied,
 }
 
 #[derive(PartialEq, Debug, Clone)]
 pub enum LoginRole {
     Admin,
-    User
+    User,
 }
 
+#[derive(Debug, Clone)]
 pub struct User {
     pub username: String,
     pub password: String,
@@ -25,36 +28,71 @@ impl User {
         Self {
             username: username.to_lowercase(),
             password: password.to_string(),
-            role
+            role,
         }
     }
 }
 
-pub fn get_users() -> [User; 2] {
-    [
+pub fn get_users() -> Vec<User> {
+    // let mut users = vec![];
+    // users.push(User::new("admin", "password", LoginRole::Admin));
+
+    vec![
         User::new("admin", "password", LoginRole::Admin),
-        User::new("boyanivskyy", "password", LoginRole::User)
+        User::new("boyanivskyy", "password", LoginRole::User),
     ]
+}
+
+fn get_admin_usernames() -> Vec<String> {
+    let users: Vec<String> = get_users()
+        .into_iter()
+        .filter(|u| u.role == LoginRole::Admin)
+        .map(|u| u.username)
+        .collect();
+
+    users
+}
+
+fn get_users_hashmap() -> HashMap<String, User> {
+    let mut users_hashmap = HashMap::new();
+
+    users_hashmap.insert(
+        "admin".to_string(),
+        User::new("admin", "password", LoginRole::Admin),
+    );
+    users_hashmap.insert(
+        "boyanivskyy".to_string(),
+        User::new("boyanivskyy", "password", LoginRole::User),
+    );
+
+    users_hashmap
 }
 
 pub fn login(username: &str, password: &str) -> Option<LoginAction> {
     let username = username.to_lowercase();
-    let users = get_users();
 
-    if let Some(user) = users.iter().find(|user| user.username == username) {
+    // NOTE: approach with vector
+    // let users = get_users();
+    // if let Some(user) = users.iter().find(|user| user.username == username) {
+
+    // NOTE: approach with hash map
+    let users = get_users_hashmap();
+    if let Some(user) = users.get(&username) {
         if user.password == password {
             return Some(LoginAction::Granted(user.role.clone()));
         } else {
             return Some(LoginAction::Denied);
         }
-    } 
+    }
 
     return None;
 }
 
 pub fn read_line() -> String {
     let mut input = String::new();
-    std::io::stdin().read_line(&mut input).expect("Std in is not working");
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Std in is not working");
 
     input.trim().to_string()
 }
@@ -62,7 +100,7 @@ pub fn read_line() -> String {
 #[cfg(test)]
 mod tests {
     // NOTE: super is the module name and ::* to include everything from the code above(some scope)
-    use super::*; 
+    use super::*;
 
     #[test]
     fn test_greet_user() {
@@ -71,9 +109,14 @@ mod tests {
 
     #[test]
     fn test_login() {
-        assert_eq!(login("admin", "password"), Some(LoginAction::Granted(LoginRole::Admin)));
-        assert_eq!(login("boyanivskyy", "password"), Some(LoginAction::Granted(LoginRole::User)));
+        assert_eq!(
+            login("admin", "password"),
+            Some(LoginAction::Granted(LoginRole::Admin))
+        );
+        assert_eq!(
+            login("boyanivskyy", "password"),
+            Some(LoginAction::Granted(LoginRole::User))
+        );
         assert_eq!(login("random", "credentials"), None);
     }
-
 }
